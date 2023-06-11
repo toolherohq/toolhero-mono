03e35ef7c08231ec33f1d7238361d5ce3537534d
Rahul T <rahulti314@gmail.com>
2023-06-11 20:01:08 +0530

    setup skeleton to handle on submit

--- packages/toolhero/src/main/valueObjects/HeroTool.ts
+++ packages/toolhero/src/main/valueObjects/HeroTool.ts
@@ -1,9 +1,11 @@
 import { ValueObject } from "../../shared/domain/ValueObject";
 import { HeroInput, IHeroInputSerialized } from "./HeroInput";
 
+export type IOnHeroToolSubmit = () => void;
 export interface IHeroToolProps {
     name: string;
     input: HeroInput;
+    onSubmit: IOnHeroToolSubmit;
 }
 export interface IHeroToolSeralised {
     name: string;
@@ -20,11 +22,18 @@ export class HeroTool extends ValueObject<IHeroToolProps> {
     public static New(name: string): HeroTool {
         return new HeroTool({
             name,
-            input: HeroInput.New()
+            input: HeroInput.New(),
+            onSubmit: () => { }
         })
     }
 
     get input(): HeroInput {
         return this.props.input;
     }
+
+    onSubmit(onHeroToolSubmit: IOnHeroToolSubmit) {
+        this.props.onSubmit = onHeroToolSubmit;
+        return this
+    }
+
 }
