f3a17a2b6ca4a3a856a5b21c6b954428193b27ea
Rahul T <rahulti314@gmail.com>
2023-07-11 09:50:16 +0530

    export output and table

--- packages/toolhero/src/index.ts
+++ packages/toolhero/src/index.ts
@@ -1,6 +1,10 @@
 import { HeroText } from './main/valueObjects/HeroText';
 import { HeroTool } from './main/valueObjects/HeroTool';
 import { HeroNextManager } from './toolhero/HeroNextHandler';
+import { HeroButton } from "./main/valueObjects/HeroButton";
+import { HeroOutput } from "./main/valueObjects/HeroOutput";
+import { HeroTable } from "./main/valueObjects/HeroTable";
+import { HeroTableRow } from "./main/valueObjects/HeroTableRow";
 
 
-export { HeroNextManager, HeroTool, HeroText }
+export { HeroNextManager, HeroTool, HeroText, HeroButton, HeroOutput, HeroTable, HeroTableRow }
